7312aacdb5ce1a21b43c8bf09a3925c98d3892d5
carlos calderon <cdcalderon@gmail.com>
2023-02-19 16:12:16 -0600

    convert trading view to funcitonal component

--- src/components/TVChartContainer/index.tsx
+++ src/components/TVChartContainer/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useEffect, useRef } from 'react';
 import './index.css';
 import {
 	widget,
@@ -25,92 +26,85 @@ export interface ChartContainerProps {
 	container: ChartingLibraryWidgetOptions['container'];
 }
 
-export interface ChartContainerState {
-}
+export function TVChartContainer(props: Partial<ChartContainerProps>): JSX.Element {
+	const ref = useRef<HTMLDivElement>(null);
 
-function getLanguageFromURL(): LanguageCode | null {
-	const regex = new RegExp('[\\?&]lang=([^&#]*)');
-	const results = regex.exec(location.search);
-	return results === null ? null : decodeURIComponent(results[1].replace(/\+/g, ' ')) as LanguageCode;
-}
+	function getLanguageFromURL(): LanguageCode | null {
+		const regex = new RegExp('[\\?&]lang=([^&#]*)');
+		const results = regex.exec(location.search);
+		return results === null
+			? null
+			: (decodeURIComponent(results[1].replace(/\+/g, ' ')) as LanguageCode);
+	}
 
-export class TVChartContainer extends React.PureComponent<Partial<ChartContainerProps>, ChartContainerState> {
-	public static defaultProps: Omit<ChartContainerProps, 'container'> = {
-		symbol: 'AAPL',
-		interval: 'D' as ResolutionString,
-		datafeedUrl: 'https://demo_feed.tradingview.com',
-		libraryPath: '/charting_library/',
-		chartsStorageUrl: 'https://saveload.tradingview.com',
-		chartsStorageApiVersion: '1.1',
-		clientId: 'tradingview.com',
-		userId: 'public_user_id',
-		fullscreen: false,
-		autosize: true,
-		studiesOverrides: {},
-	};
-
-	private tvWidget: IChartingLibraryWidget | null = null;
-	private ref: React.RefObject<HTMLDivElement> = React.createRef();
-
-	public componentDidMount(): void {
-		if (!this.ref.current) {
+	useEffect(() => {
+		if (!ref.current) {
 			return;
 		}
 
+		console.log('carlos', (window as any).TradingView);
+		console.log('carlos', props.datafeedUrl);
 		const widgetOptions: ChartingLibraryWidgetOptions = {
-			symbol: this.props.symbol as string,
+			symbol: props.symbol as string,
 			// BEWARE: no trailing slash is expected in feed URL
 			// tslint:disable-next-line:no-any
-			datafeed: new (window as any).Datafeeds.UDFCompatibleDatafeed(this.props.datafeedUrl),
-			interval: this.props.interval as ChartingLibraryWidgetOptions['interval'],
-			container: this.ref.current,
-			library_path: this.props.libraryPath as string,
+			datafeed: new (window as any).Datafeeds.UDFCompatibleDatafeed(props.datafeedUrl),
+
+			interval: props.interval as ChartingLibraryWidgetOptions['interval'],
+			container: ref.current,
+			library_path: props.libraryPath as string,
 
 			locale: getLanguageFromURL() || 'en',
 			disabled_features: ['use_localstorage_for_settings'],
 			enabled_features: ['study_templates'],
-			charts_storage_url: this.props.chartsStorageUrl,
-			charts_storage_api_version: this.props.chartsStorageApiVersion,
-			client_id: this.props.clientId,
-			user_id: this.props.userId,
-			fullscreen: this.props.fullscreen,
-			autosize: this.props.autosize,
-			studies_overrides: this.props.studiesOverrides,
+			charts_storage_url: props.chartsStorageUrl,
+			charts_storage_api_version: props.chartsStorageApiVersion,
+			client_id: props.clientId,
+			user_id: props.userId,
+			fullscreen: props.fullscreen,
+			autosize: false,
+			studies_overrides: props.studiesOverrides,
 		};
 
 		const tvWidget = new widget(widgetOptions);
-		this.tvWidget = tvWidget;
-
 		tvWidget.onChartReady(() => {
 			tvWidget.headerReady().then(() => {
 				const button = tvWidget.createButton();
 				button.setAttribute('title', 'Click to show a notification popup');
 				button.classList.add('apply-common-tooltip');
-				button.addEventListener('click', () => tvWidget.showNoticeDialog({
+				button.addEventListener('click', () =>
+					tvWidget.showNoticeDialog({
 						title: 'Notification',
 						body: 'TradingView Charting Library API works correctly',
 						callback: () => {
 							console.log('Noticed!');
 						},
-					}));
+					}),
+				);
 				button.innerHTML = 'Check API';
 			});
 		});
-	}
 
-	public componentWillUnmount(): void {
-		if (this.tvWidget !== null) {
-			this.tvWidget.remove();
-			this.tvWidget = null;
-		}
-	}
+		return () => {
+			if (tvWidget !== null) {
+				tvWidget.remove();
+			}
+		};
+	}, [props]);
 
-	public render(): JSX.Element {
-		return (
-			<div
-				ref={ this.ref }
-				className={ 'TVChartContainer' }
-			/>
-		);
-	}
+	return <div ref={ref} className={'TVChartContainer'} />;
 }
+
+TVChartContainer.defaultProps = {
+	symbol: 'AAPL',
+	interval: 'D' as ResolutionString,
+	datafeedUrl: 'https://demo_feed.tradingview.com',
+	libraryPath: '/charting_library/',
+	chartsStorageUrl: 'https://saveload.tradingview.com',
+	chartsStorageApiVersion: '1.1',
+	clientId: 'tradingview.com',
+	userId: 'public_user_id',
+	fullscreen: false,
+	autosize: true,
+	studiesOverrides: {},
+};
